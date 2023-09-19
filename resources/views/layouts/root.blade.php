14bbfb9b76e3085d2514132bf90bbbb3d617f24d
aufarijaal <rijalaufa6@gmail.com>
2023-09-19 12:13:11 +0700

    fix theme toggling

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
     x-data="{
-        darkMode: localStorage.getItem('darkMode')
-        || localStorage.setItem('darkMode', 'system')}"
-    x-init="$watch('darkMode', val => localStorage.setItem('darkMode', val))"
-    x-bind:class="{'dark': darkMode === 'dark' || (darkMode === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)}"
+        theme: localStorage.getItem('theme')
+        || localStorage.setItem('theme', 'system')}"
+    x-init="$watch('theme', val => localStorage.setItem('theme', val))"
+    x-bind:class="{'dark': theme === 'dark' || (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)}"
 >
 
 <head>
